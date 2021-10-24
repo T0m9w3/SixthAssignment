e277da29106dc09a36b8bc892e48ff3ca01f2643
T0m9w3 <trourke@stduent.bridew.edu>
2021-10-24 12:00:25 -0400

    Recently Updated Commit Version 1

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,25 +1,41 @@
 package com.company;
 
 public class BankAccount {
-    private double balance;
-    private float interestRate;
+    private double Balance;
+    private float InterestRate;
+    private int AccountID;
+    private static int NextID = 1000;
 
+    public BankAccount(){
+        InterestRate = 0.02f;
+        AccountID = NextID;
+        NextID = NextID+1;
+    }
+    public BankAccount(double initialBalance, float initialInterestRate){
+        Balance = initialBalance;
+        InterestRate = initialInterestRate;
+        AccountID = NextID;
+        NextID++;
+    }
+    public int getAccountID(){
+        return AccountID;
+    }
     public void deposit(double amount){
-        balance += amount;
+        Balance += amount;
     }
     public boolean withdraw(double amount){
-        if (amount > balance){
+        if (amount > Balance){
             return false;
         }
         else
-            balance = balance - amount;
+            Balance = Balance - amount;
         return true;
     }
     public double checkBalance(){
-        return balance;
+        return Balance;
     }
     public double addInterest(){
-        balance += interestRate*balance;
-        return balance;
+        Balance += InterestRate*Balance;
+        return Balance;
     }
 }
