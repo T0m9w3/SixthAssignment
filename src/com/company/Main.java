0a65dced4595aee9805d227803e31bb74ee665a1
T0m9w3 <trourke@stduent.bridew.edu>
2021-10-24 12:32:04 -0400

    Recently Updated Commit Version 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        var Account = new BankAccount();
+        var Account2 = new BankAccount(10000, 0.05f);
+        Account.deposit(1000);
+        Account.addInterest();
+        var Succeeded = Account.withdraw(2000);
+        if(Succeeded){
+            System.out.println("You managed to withdraw");
+        }
+        else{
+            System.out.println("Couldn't withdraw 2000 from Account: " + Account.getAccountID() + " when you only have " + Account.checkBalance() + " in your Account");
+        }
+        Account2.addInterest();
+        System.out.println("The second Account (AccountID# " + Account2.getAccountID() + ") currently has a new Current Balance of " + Account2.checkBalance() + " after the InterestRate was added to the Account's Initial Balance");
+        Account2.withdraw(5000);
+        System.out.println("After the withdrawal of the amount that was withdraw from the second Account currently there is " + Account2.checkBalance() + " remaining in the second Account as its Current Balance");
     }
 }
